c3b92a93fcfc3e4f9b52dc0011c8388bb501fe3c
Ahmed <Ammed@Ali>
2025-10-22 14:12:45 +0300

    feat(env): integrated @nestjs/config for environment variables managements

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT', 3000);
+  const ENVIRONMENT = configService.get<string>('NODE_ENV', 'Development');
+
+  await app.listen(PORT);
+  console.log(
+    `Application is running on port ${PORT}, Environmet: ${ENVIRONMENT}, Test: http://localhost:3000/`,
+  );
 }
-bootstrap();
+void bootstrap();
